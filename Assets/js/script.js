e8f62937932201765e7d2d471c259f45ae134208
Sathya2346 <ganesansathya2346@gmail.com>
2025-12-26 21:44:49 +0530

    script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,13 @@
-// script.js
-// Nav Bar
 
-let togg = document.querySelector('.togg');
-let media = document.querySelector('.media');
-let CloseIcon = document.querySelector('.CloseIcon');
-CloseIcon.addEventListener('click',function(){
-  media.classList.remove('mediaActive');
-});
-togg.addEventListener('click', function(){
-    media.classList.toggle('mediaActive');
-});
-
-            
             // Send
 function send(){
-  let name = document.getElementById('name').value.length;
-  let email = document.getElementById('email').value.length;
+    let name = document.getElementById('name').value.length;
+    let email = document.getElementById('email').value.length;
 
-  if(name >= 4 && email >= 4){
-    alert("SuccessFully Message Send");
-  }
-  else{
-    alert('Please Enter Valid Inputs');
-  }
+    if(name >= 4 && email >= 4){
+        alert("SuccessFully Message Send");
+    }
+    else{
+        alert('Please Enter Valid Inputs');
+    }
 }
